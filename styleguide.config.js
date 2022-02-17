61c7d634338c96ccaa871d7b4bc33706b0911886
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2022-02-17 16:07:11 -0500

    Add files via upload

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   components: "src/components/**/[A-Z]*.tsx",
+  ignore: [
+    "**/__tests__/**",
+    "**/*.test.{js,jsx,ts,tsx}",
+    "**/*.spec.{js,jsx,ts,tsx}",
+    "**/*.stories.{js,jsx,ts,tsx}",
+    "**/*.d.ts",
+  ],
 };
